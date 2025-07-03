8e6a8aeb3e19936dfcf9185d5de0462b393ff0e4
unknown <onurbayirli17@gmail.com>
2025-07-03 23:19:52 +0300

    STATIC ROOT added to settings.py

--- pskensar/settings.py
+++ pskensar/settings.py
@@ -138,6 +138,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [STATIC_DIR,]
 
 MEDIA_URL = '/media/'
